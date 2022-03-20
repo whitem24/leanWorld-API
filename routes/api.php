260f0dc58471079de9e1c48d53d5c0cd1a945e82
jonathankumar10 <jonathanpasupulety@gmail.com>
2022-03-19 21:02:44 -0500

    Implemented profiles controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// //Importing profiles controller
+// use App\Http\Controllers\ProfilesController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes

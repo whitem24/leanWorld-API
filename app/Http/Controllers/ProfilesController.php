75102e64e6cf6a77d345df9d65a039332276bd62
jonathankumar10 <jonathanpasupulety@gmail.com>
2022-04-22 17:40:23 -0500

    Added upload image in base64 to database encode

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -74,48 +74,46 @@ class ProfilesController extends Controller
 
     public function update(Request $request)
     {   
-       return response()->json($request,200);
-         
-    }
-    //     $validator = Validator::make($request->all(),[
-    //         'username' => 'required|max:191',
-    //         'first_name' => 'required|max:191',
-    //         'organization_name' => 'required|max:191',
-    //         'email' => 'required|max:191',
-    //         'number' => 'required|max:191',
-    //         'birthday' => 'required|max:191',
-    //         'image' => 'required|image|mimes:jpeg,png,jpg|max:5048',
-    //     ]);
+        $validator = Validator::make($request->all(),[
+            'username' => 'required|max:191',
+            'first_name' => 'required|max:191',
+            'organization_name' => 'required|max:191',
+            'email' => 'required|max:191',
+            'number' => 'required|max:191',
+            'birthday' => 'required|max:191',
+        ]);
 
-    //     if($validator->fails()){
-    //         return response()->json([
-    //             'error' => $validator->messages()], 404);
-    //     }
-    //     else{
-    //     $id = Auth::user()->id;
-    //     $data = Profile::where('user_id',$id)->first();
+        if($validator->fails()){
+            return response()->json([
+                'error' => $validator->messages()], 404);
+        }
+        else{
+        $id = Auth::user()->id;
+        $data = Profile::where('user_id',$id)->first();
 
-    //     $data->username =$request->input('username'); 
-    //     $data->first_name =$request->input('first_name');
-    //     $data->last_name =$request->input('last_name');
-    //     $data->organization_name =$request->input('organization_name');
-    //     $data->location =$request->input('location');
-    //     $data->email =$request->input('email');
-    //     $data->number =$request->input('number');
-    //     $data->birthday =$request->input('birthday');
+        $data->username =$request->input('username'); 
+        $data->first_name =$request->input('first_name');
+        $data->last_name =$request->input('last_name');
+        $data->organization_name =$request->input('organization_name');
+        $data->location =$request->input('location');
+        $data->email =$request->input('email');
+        $data->number =$request->input('number');
+        $data->birthday =$request->input('birthday');
 
-    //     if($request->hasFile('profile_picture')){
+        if($request->input('profile_picture')){
+            $file = $request->input('profile_picture');
+            $folderPath = "uploads/profiles/";
+            $base64Image = explode(";base64,",$file);
+            $explodeImage = explode("image/",$base64Image[0]);
+            $imageType = $explodeImage[1];
+            $image_base64 = \base64_decode($base64Image[1]);
+            $data->profile_picture = $folderPath . uniqid() .'.'.$imageType;
 
-    //         $file = $request->file('profile_picture');
-    //         $extension = $file->getClientOriginalExtension();
-    //         $filename = time().' . '.$extension;
-    //         $file->move('uploads/profiles/',$filename);
-    //         $data->profile_picture = 'uploads/profiles/'.$filename;
-    //     }
+            file_put_contents( $data->profile_picture, $image_base64);
+        }
     
-    //     $data->update();
-    //     return response()->json(['message' => 'Successfully updated Profile!'], 200);
-    //     }
-    // }
-   
+        $data->update();
+        return response()->json(['message' => 'Successfully updated Profile!'], 200);
+        }
+    }
 }
